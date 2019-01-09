46f51fb394e79a5d44e4f583576b23e3efa224ba
YA21 <ytik2013k@gmail.com>
2019-01-09 22:51:07 +0900

    adjust changed functions

--- cnn_runner.py
+++ cnn_runner.py
@@ -1,12 +1,15 @@
 from models.cnn import cnn_model
 from utils.preprocess_images import preprocess_images
+from utils.split_data import split_data
 
 if __name__ == '__main__':
-    X_train, X_test, y_train, y_test = preprocess_images(images_dir="./images")
+    image_dir = "./images"
+    X, Y = preprocess_images(images_dir=image_dir)
+    X_train, X_test, y_train, y_test = split_data(X, Y, test_size=0.2)
     model = cnn_model(input_shape=X_train.shape[1:], num_classes=y_train.shape[1])
     model.fit(X_train, y_train,
               batch_size=128,
-              epochs=20,
+              epochs=2,
               verbose=1,
               validation_data=(X_test, y_test))
     scores = model.evaluate(X_test, y_test, verbose=1)
