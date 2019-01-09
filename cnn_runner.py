7a303079613f30889d09f67459aaea0be433d26c
YA21 <ytik2013k@gmail.com>
2019-01-09 23:26:50 +0900

    adopt binary classification and print network summary

--- cnn_runner.py
+++ cnn_runner.py
@@ -3,15 +3,27 @@ from utils.preprocess_images import preprocess_images
 from utils.split_data import split_data
 
 if __name__ == '__main__':
-    image_dir = "./images"
-    X, Y = preprocess_images(images_dir=image_dir)
+    train_image_dir = "./images"
+    X, Y, class_index = preprocess_images(images_dir=train_image_dir)
     X_train, X_test, y_train, y_test = split_data(X, Y, test_size=0.2)
-    model = cnn_model(input_shape=X_train.shape[1:], num_classes=y_train.shape[1])
+
+    # if you're doing binary classification, only one output layer is enough
+    if len(class_index)==2:
+        output_layer_num = 1
+        y_train = y_train[:,0]
+        y_test = y_test[:,0]
+    else:
+        output_layer_num = y_train.shape[1]
+
+    model = cnn_model(input_shape=X_train.shape[1:], output_layer_num=output_layer_num)
     model.fit(X_train, y_train,
               batch_size=128,
               epochs=2,
               verbose=1,
               validation_data=(X_test, y_test))
+
     scores = model.evaluate(X_test, y_test, verbose=1)
     print("valid loss:", scores[0])
     print("valid accuracy", scores[1])
+    print("model summary:")
+    model.summary()
