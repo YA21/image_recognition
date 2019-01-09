a3ad394a1d592a9b6329f40f17d4b0d882b0c305
YA21 <ytik2013k@gmail.com>
2019-01-09 23:26:01 +0900

    adopt binary classification

--- models/cnn.py
+++ models/cnn.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 
-def cnn_model(input_shape, num_classes):
+def cnn_model(input_shape, output_layer_num):
     model = Sequential()
 
     model.add(Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding="same", input_shape=input_shape))
@@ -16,9 +16,12 @@ def cnn_model(input_shape, num_classes):
     model.add(Dense(512))
     model.add(Activation("relu"))
     model.add(Dropout(0.5))
-    model.add(Dense(num_classes))
+    model.add(Dense(output_layer_num))
     model.add(Activation("softmax"))
 
-    model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
+    if output_layer_num == 1:
+        model.compile(loss="binary_crossentropy", optimizer="SGD", metrics=["accuracy"])
+    else:
+        model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
 
     return model
